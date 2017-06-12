e8d0dd6b3e71fa93dc1d393d342610e6d62eb37c
ToanNguyenP <toan.nguyenp@s3corp.com.vn>
2017-06-12 20:04:40 +0700

    sss

--- js/controller/contact_controller.js
+++ js/controller/contact_controller.js
@@ -1,4 +1,16 @@
-app.controller('ContactController', ['$scope', function ($scope) {
+app.controller('ContactController', function ($scope, $http) {
 
- 	
-}]);
+	$scope.contact = {};
+ 	$scope.sendContact = function(){
+ 		$http({
+ 			method: 'POST',
+ 			url : BASE_URL + 'contact/create',
+ 			data: $.param($scope.contact),
+ 			headers : {'Content-Type': 'application/x-www-form-urlencoded'} 
+ 		}).then(function success(data){
+ 			if(data.data.status){
+ 				$scope.contact = {};
+ 			}
+ 		});
+ 	}
+});
