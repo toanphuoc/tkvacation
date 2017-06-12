0d4322b459d771a3eb15f0b8ce70c5a3fa662c6b
ToanNguyenP <toan.nguyenp@s3corp.com.vn>
2017-06-13 02:06:40 +0700

    implementation customize tour

--- js/controller/contact_controller.js
+++ js/controller/contact_controller.js
@@ -2,6 +2,28 @@ app.controller('ContactController', function ($scope, $http) {
 
 	$scope.contact = {};
  	$scope.sendContact = function(){
+ 		var regExpPhoneNumber = /^[0-9()-]+$/;
+ 		var regExpEmail = /^.+@.+\..+$/;
+ 		if($scope.contact.name == undefined || $scope.contact.name === ''){
+ 			$scope.full_name_error = true;
+ 			return;
+ 		}else $scope.full_name_error = false;
+
+ 		if($scope.contact.number == undefined || $scope.contact.number === '' || !$scope.contact.number.match(regExpPhoneNumber)){
+ 			$scope.number_error = true;
+ 			return;
+ 		}else $scope.number_error = false;
+
+ 		if($scope.contact.email == undefined || $scope.contact.email === '' || !$scope.contact.email.match(regExpEmail)){
+ 			$scope.email_error = true;
+ 			return;
+ 		}else $scope.email_error = false;
+
+ 		if($scope.contact.message == undefined || $scope.contact.message === ''){
+ 			$scope.message_error = true;
+ 			return;
+ 		}else $scope.message_error = false;
+
  		$http({
  			method: 'POST',
  			url : BASE_URL + 'contact/create',
@@ -10,6 +32,9 @@ app.controller('ContactController', function ($scope, $http) {
  		}).then(function success(data){
  			if(data.data.status){
  				$scope.contact = {};
+ 				$scope.message_success = true;
+
+ 				setTimeout(function(){ $('.message_success').addClass('hidden'); }, 5000);
  			}
  		});
  	}
--- js/controller/customize_controller.js
+++ js/controller/customize_controller.js
@@ -1,4 +1,31 @@
-app.controller('CustumizeTourController', function ($scope) {
+app.controller('CustumizeTourController', function ($scope, $http) {
+	$('#datepicker').datepicker({
+		autoSize: true, 
+		closeText: "Close", 
+		defaultDate: new Date(),
+		dateFormat: 'yy-mm-dd'
+	});
 
- 
+	$scope.customize = {};
+
+	$scope.send = function(){
+		$scope.submitted = true;
+		 if(!$scope.customize_form.$valid) {
+	       return;
+	     }
+		$http({
+			method: 'POST',
+ 			url : BASE_URL + 'customizetour/create',
+ 			data: $.param($scope.customize),
+ 			headers : {'Content-Type': 'application/x-www-form-urlencoded'} 
+		}).then(function success(response){
+			if(response.data.status){
+ 				$scope.customize = {};
+ 				$scope.message_success = true;
+ 				$scope.submitted = false;
+
+ 				setTimeout(function(){ $('.message_success').addClass('hidden'); }, 5000);
+ 			}
+		});
+	}
 });
