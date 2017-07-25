d9d3a5096308e3ae8a5b81aa8cf857cc7ce4c1d5
ToanNguyenP <toan.nguyenp@s3corp.com.vn>
2017-07-25 19:13:22 +0700

    dadas

--- js/controller/blog_detail_controller.js
+++ js/controller/blog_detail_controller.js
@@ -1,3 +1,14 @@
 app.controller('BlogDetailController', function ($scope) {
 
+	$('.carousel').carousel({
+	    interval: 5000 //changes the speed
+	});
+
+	$scope.prevCarouel = function(){
+		$('.carousel').carousel('prev');
+	}
+
+	$scope.nextCarouel = function(){
+		$('.carousel').carousel('next');
+	}
 });
